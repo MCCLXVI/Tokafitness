eb988fb6b2afef2e8e082bd1ee5328b4dba606c0
MCCLXVI <144335553+MCCLXVI@users.noreply.github.com>
2025-01-27 15:59:16 +0000

    Please commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav className='headernav'>
+          <div className='title-container'>
+          <img className='' src="" alt="" />
+            <h1>Toka Fitness</h1>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
